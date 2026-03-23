97b452e38e17ccc131f7821820ff1ebe9c46ee1b
Wanderson Praxede <wspraxede@gmail.com>
2026-03-22 21:05:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function calcular(){
 
+let garrafas = document.getElementById("garrafas").value || 0;
+let sacolas = document.getElementById("sacolas").value || 0;
+let embalagens = document.getElementById("embalagens").value || 0;
+
+let totalSemana = Number(garrafas) + Number(sacolas) + Number(embalagens);
+let totalAno = totalSemana * 52;
+
+document.getElementById("resultado").innerText =
+"Você utiliza aproximadamente " + totalAno + " itens de plástico por ano.";
+}
